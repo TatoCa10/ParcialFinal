a0e4af9aff5f233eb7ef882394928b2df6f85c8b
Carlos Alberto <Carlos Alberto@Lenovo-PC>
2017-06-01 18:03:32 -0500

    BAR3D.

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -57,9 +57,9 @@ public class ChartServlet extends HttpServlet {
             String id2= (String)Result.get(2);
         
             DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(x, "Series 1", "ID");   
+        dataset.addValue(x, "Series 1", "Category 1");   
         //dataset.addValue(x2, "Series 1", "Category 2");   
-        dataset.addValue(x2, "Series 2", "ID");   
+        dataset.addValue(x2, "Series 2", "Category 1");   
         //dataset.addValue(29.0, "Series 2", "Category 2");   
         //dataset.addValue(41.0, "Series 3", "Category 1");   
         //dataset.addValue(33.0, "Series 3", "Category 2");   
